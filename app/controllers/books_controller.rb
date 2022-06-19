30972b54238a17e90eb66e414842a8e1b3666da1
EC2 Default User <kazukipump7@gmail.com>
2022-06-19 04:21:25 +0000

    resubmit bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,47 @@
 class BooksController < ApplicationController
+
+  def new
+  end
+
   def index
     @book = Book.new
     @books = Book.all
   end
 
-  def edit
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render:index
+    end
   end
 
+
   def show
+    @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+end
+
+
+# ストロングパラメイター
+
+def book_params
+  params.require(:book).permit(:title, :body)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # get '/top' => 'homes#top'
   root 'homes#top'
 
-  get 'books/index' => 'books#index', as: :books
-  get 'books/edit'
-  get 'books/show'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
